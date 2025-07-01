2e7a61f05969c3a225011e5e111adf447d2b085a
Ton Tan Khang <87051539+KhangToon@users.noreply.github.com>
2025-07-01 23:29:25 +0700

    Add project files.

---
+++ eCommerce.SharedLibrary/Response/Response.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.SharedLibrary.Responses
+{
+    public record class Response (bool Flage = false, string? Message = null);
+}
