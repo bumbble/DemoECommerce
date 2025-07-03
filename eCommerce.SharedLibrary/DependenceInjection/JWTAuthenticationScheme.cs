bcc2f275f2114d9cfc42ee82f648362df09ca9c4
Ton Tan Khang <87051539+KhangToon@users.noreply.github.com>
2025-07-03 22:24:09 +0700

    Create JWT Authentication Scheme

---
+++ eCommerce.SharedLibrary/DependenceInjection/JWTAuthenticationScheme.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.SharedLibrary.DependenceInjection
+{
+    public class JWTAuthenticationScheme
+    {
+        public static IServiceCollection AddJWTAuthentication(IServiceCollection services, IConfiguration configuration)
+        {
+            // Configure JWT authentication
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer("Bearer", options =>
+                {
+                    var key = Encoding.UTF8.GetBytes(configuration.GetSection("Authentication: Key").Value!);
+                    string issuer = configuration.GetSection("Authentication: Issuer").Value!;
+                    string audience = configuration.GetSection("Authentication: Audience").Value!;
+                    options.RequireHttpsMetadata = false; // Set to true in production
+                    options.SaveToken = true;
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = issuer,
+                        ValidAudience = audience,
+                        IssuerSigningKey = new SymmetricSecurityKey(key)
+                    };
+                });
+
+            return services;
+        }
+    }
+}
